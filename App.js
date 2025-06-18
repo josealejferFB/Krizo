57c0558030bd4c45e787b75b8625a9591c6599ae
josealejferFB <josealejfer@gmail.com>
2025-06-17 22:14:30 -0400

    primer comit
    hpña

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{ color: "white", fontSize: 40 }}>¡Hola, Esto es Krizo!</Text>
+      <Text>Hola</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#f25101",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
