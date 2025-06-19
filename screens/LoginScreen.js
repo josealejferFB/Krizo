71b6fe69f0bfcb0c8e7c7da89263202df1d72f4c
josealejferFB <josealejfer@gmail.com>
2025-06-18 21:04:02 -0400

    fix ui 2

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Text, TextInput } from 'react-native-paper';
+import { StyleSheet, View } from 'react-native'; // Asegúrate de que View esté aquí y Text NO
+import { Text, TextInput } from 'react-native-paper'; // ¡Asegúrate que Text se importa de react-native-paper!
 import { ThemedInput, ThemedButton } from '../components/ThemedUIElements';
 import ThemedBackgroundGradient from '../components/ThemedBackgroundGradient';
+import Logo from "../assets/logo.svg";
 
 export default function LoginScreen({ navigation }) {
   const [username, setUsername] = React.useState('');
@@ -10,6 +11,11 @@ export default function LoginScreen({ navigation }) {
 
   return (
     <ThemedBackgroundGradient>
+      {/* Logo en la parte superior central */}
+      <View style={styles.logoContainer}>
+        <Logo width={150} height={150} />
+      </View>
+
       <Text style={styles.title}>¡Bienvenido! Inicia Sesión</Text>
 
       <ThemedInput
@@ -35,11 +41,27 @@ export default function LoginScreen({ navigation }) {
       >
         Ingresar
       </ThemedButton>
+
+      {/* Botón "Ingresar como KrizoWorker" */}
+      <ThemedButton
+        onPress={() => {
+          console.log('Ingresando como KrizoWorker');
+          navigation.replace('Home');
+        }}
+        style={styles.krizoWorkerButton}
+        labelStyle={styles.krizoWorkerButtonLabel}
+        mode="contained"
+      >
+        Ingresar como KrizoWorker
+      </ThemedButton>
     </ThemedBackgroundGradient>
   );
 }
 
 const styles = StyleSheet.create({
+  logoContainer: {
+    marginBottom: 20,
+  },
   title: {
     fontSize: 28,
     marginBottom: 30,
@@ -47,4 +69,15 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     color: '#FFFFFF',
   },
+  krizoWorkerButton: {
+    height: 70,
+    marginTop: 20,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 12,
+  },
+  krizoWorkerButtonLabel: {
+    color: '#FC5501',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
 });
